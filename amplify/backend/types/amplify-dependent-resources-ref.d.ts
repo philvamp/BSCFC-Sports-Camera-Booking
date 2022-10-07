9ff41d03e47adc9fa2333676f33935fb78dff49f
phil vamplew <philvamplew@icloud.com>
2022-10-07 15:17:22 +0100

    Add Auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyapp11e46627fc": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
